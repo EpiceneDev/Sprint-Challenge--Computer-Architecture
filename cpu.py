947218f8921d53314d09aa60470b592c3441ca3d
EpiceneDev <48662221+EpiceneDev@users.noreply.github.com>
2020-08-15 10:35:25 -0700

    alu and ldi made

--- cpu.py
+++ cpu.py
@@ -17,3 +17,20 @@ class CPU:
         self.running = True
         self.pc = 0
         self.fl = 0b00000000
+
+    def alu(self, ir, reg_a, reg_b):
+        if ir == "CMP":
+            self.fl &= 0b00000000
+            if self.reg[reg_a] == self.reg[reg_b]:
+                self.fl = 0b00000001
+            elif self.reg[reg_a] < self.reg[reg_b]:
+                self.fl = 0b00000100
+            elif self.reg[reg_a] > self.reg[reg_b]:
+                self.fl = 0b00000010
+            self.pc += 3
+
+    def ldi(self, address, value):
+        self.reg[address] = value
+        self.pc += 3
+
+    
